470406903374d5b372009205031c1eda08115c4e
Sergio Cabrera <scabrera.main@gmail.com>
2024-09-18 21:29:43 +0200

    more functions

--- front_end/src/app/app.component.ts
+++ front_end/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ThemeService } from './services/theme/theme.service';
 
 
 @Component({
@@ -12,7 +13,20 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] // Corregido de styleUrl a styleUrls
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
+
+  constructor(
+    private themeService: ThemeService
+  ){
+
+  }
+  ngAfterViewInit(): void {
+    this.themeService.setTheme('dark');
+  }
+
+  ngOnInit(): void {
+   
+  }
 
  
 }
--- front_end/src/app/services/theme/theme.service.ts
+++ front_end/src/app/services/theme/theme.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class ThemeService {
 
   constructor() { }
+  
 
   setTheme(theme: string){
     document.body.classList.remove('dark-theme', 'light-theme');
